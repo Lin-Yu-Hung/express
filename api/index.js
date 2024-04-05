1356c571a0b81f520c4fd176134d25f9066332b2
林昱鋐 <yuhunglin@lafresh.com.tw>
2024-04-05 16:45:51 +0800

    新增確認交易流程、優化程式碼

--- api/index.js
+++ api/index.js
@@ -6,9 +6,23 @@ const cors = require('cors')
 const app = express();
 const PORT = 3000;
 
-const ChannelSecret = "080c23a52d12238f48e2d38044a2a09d";
 app.use(express.json());
 app.use(cors())
+
+
+const createHeader = (uri, params) => {
+    const ChannelSecret = "080c23a52d12238f48e2d38044a2a09d";
+    const nonce = parseInt(new Date().getTime() / 1000);
+    const string = `${ChannelSecret}${uri}${JSON.stringify(params)}${nonce}`;
+    const hmacDigest = Base64.stringify(HmacSHA256(string, ChannelSecret));
+
+    return {
+        "Content-Type": "application/json",
+        'X-LINE-ChannelId': '2004505560',
+        'X-LINE-Authorization': hmacDigest,
+        "X-LINE-Authorization-Nonce": nonce
+    }
+}
 
 // 處理 POST 請求
 app.post('/linepay/request', async (req, res) => {
@@ -16,20 +30,23 @@ app.post('/linepay/request', async (req, res) => {
     const requestBody = req.body;
     console.log("🚀  requestBody:", requestBody)
     const requestUri = "/v3/payments/request";
-    const nonce = parseInt(new Date().getTime() / 1000);
-
-    const string = `${ChannelSecret}${requestUri}${JSON.stringify(requestBody)}${nonce}`;
-    const hmacDigest = Base64.stringify(HmacSHA256(string, ChannelSecret));
-
-    const headers = {
-        "Content-Type": "application/json",
-        'X-LINE-ChannelId': '2004505560',
-        'X-LINE-Authorization': hmacDigest,
-        "X-LINE-Authorization-Nonce": nonce
-    };
-
+    const headers = createHeader(requestUri, requestBody);
     try {
-        const response = await axios.post("https://sandbox-api-pay.line.me/v3/payments/request", requestBody, { headers });
+        const response = await axios.post(`https://sandbox-api-pay.line.me${requestUri}`, requestBody, { headers });
+        res.json(response.data);
+    } catch (error) {
+        res.status(error.response.status).json(error.response.data);
+    }
+});
+app.post('/payments/confirm', async (req, res) => {
+    const requestBody = req.body;
+    console.log("🚀  requestBody:", requestBody)
+    const { transactionId, amount, currency } = requestBody;
+    const requestUri = `/v3/payments/${transactionId}/confirm`;
+    const params = { amount, currency };
+    const headers = createHeader(requestUri, params);
+    try {
+        const response = await axios.post(`https://sandbox-api-pay.line.me${requestUri}`, params, { headers });
         res.json(response.data);
     } catch (error) {
         res.status(error.response.status).json(error.response.data);
